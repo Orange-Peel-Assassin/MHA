286dafa12414f3f0e5b7ef3ca5d2cbcf52878938
Orange-Peel-Assassin <56128431+Orange-Peel-Assassin@users.noreply.github.com>
2021-10-26 21:30:49 -0700

    Split NPCs and PC

--- js/NPC.js
+++ js/NPC.js
@@ -6,20 +6,36 @@ $(document).ready(function() {
     var name = params.get("name");
 
     Promise.all([
-        d3.csv("../../Raw Chars/AllChars.csv")
+        d3.csv("../../Raw Chars/AllChars.csv", (row) => {
+            row.group = (row.player === "GM")? "NPC": "PC";
+            return row;
+        })
     ]).then(([parsedCharacters]) => {
         characters = parsedCharacters;
+        charByPlayer = d3.group(characters, d => d.group);
+        let charListDiv = $("#char-lists");
+        charByPlayer.forEach((v, k) => {
+            let charGroup = $("<div/>");
+            charGroup.append($(`<h2>${k}</h2>`));
+
+            let charUl = $("<ul/>");
+            charGroup.append(charUl);
+                v.forEach(playerChar => {
+                    charUl.append($(`<li><a href="?name=${playerChar.title}">${playerChar.title}</a></li>`));
+                });
+            charListDiv.append(charGroup);
+        });
+
         if (name === null){
             $("#npc-page").hide();
-
-            let pcList = $("#pc-list");
-            characters.forEach(c => {
+                let pcList = $("#pc-list");
+            /*characters.forEach(c => {
                 let pclink = $("<a/>")
                     .attr("href", `?name=${c.title}`)
                     .text(c.title);
                 pcList.append($("<li/>")
                     .html(pclink));
-            });
+            });*/
         }
         else{
             name = name.toLowerCase();
@@ -49,11 +65,18 @@ function ShowCharInfo(char){
             .text(c));
     });
     $("#info-box-player").text(char.player);
-    if (character["photo"] === null){
-        $("#info-box-img").attr("src", `../../SVG/MHA-discord-seeklogo.svg`);
-    }
-    else{
-        $("#info-box-img").attr("src", `${char.photo}`);
-    }
+    $("#info-box-img").attr("src",
+        character.photo === ""? 
+            "../../SVG/MHA-discord-seeklogo.svg":
+            `${char.photo}` );
+    $("#page-appearance").text(char.appearance);
+    $("#page-quirk").text(char.quirkInfo);
+    $("#page-backround").text(char.backround);
+    let noteArray = character["notes"].split(",");
+    noteArray.forEach(c => {
+        $("#page-notes")
+            .append($("<li/>")
+            .text(c));
+    });
 
 }
