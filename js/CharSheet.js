32db98a3875c8e15ce1d6b18681322d53d965c62
Orange-Peel-Assassin <56128431+Orange-Peel-Assassin@users.noreply.github.com>
2022-02-08 20:05:53 -0700

    Fixed non-zero indexes

--- js/CharSheet.js
+++ js/CharSheet.js
@@ -3,14 +3,21 @@ $(document).ready(function() {
     loadCsv().then(() => {
         name = name.toLowerCase();
         character = characters.find(c => c.title.toLowerCase() === name);
+        if (character.name !== "N/A" && character.name !== null){
+            $("#charSheet-name").html(character.name);
+        }
+        else{
+            $("#charSheet-name").html(character.title);
+        }
         $("#charSheet-backround").html(character.backround);
         $("#charSheet-blood").html(character.bloodType);
-        $("#charSheet-name").html(character.title);
         $("#charSheet-age").html(character.age);
+        $("#charSheet-alias").html(character.alias);
         $("#charSheet-quirk").html(character.quirkInfo);
         $("#charSheet-class").html(character.class);
         $("#charSheet-quirkType").html(character.quirkType);
         $("#charSheet-equipment").html(character.equipment);
+        $("#charSheet-stat-0").html(character.stats[0]);
         $("#charSheet-stat-1").html(character.stats[1]);
         $("#charSheet-stat-2").html(character.stats[2]);
         $("#charSheet-stat-3").html(character.stats[3]);
@@ -18,7 +25,6 @@ $(document).ready(function() {
         $("#charSheet-stat-5").html(character.stats[5]);
         $("#charSheet-stat-6").html(character.stats[6]);
         $("#charSheet-stat-7").html(character.stats[7]);
-        $("#charSheet-stat-8").html(character.stats[8]);
         PhotoToHTML("charSheet-photo",character);
         erosFunction();
     });
